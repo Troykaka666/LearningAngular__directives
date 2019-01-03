f11329a81e94caf57527abb61be4c2302603b563
Troy <Troywu9527@gmail.com>
2019-01-03 13:11:25 -0500

    Finish deeper directives section

---
+++ src/app/unless.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appUnless]'
+})
+export class UnlessDirective {
+
+  //have to make sure the name is same as the selector
+  @Input() set appUnless(condition: boolean){
+    if (!condition) {
+      this.vcRef.createEmbeddedView(this.templateRef);
+    }else{
+      this.vcRef.clear();
+    }
+  }
+  constructor(private templateRef: TemplateRef<any>, private vcRef: ViewContainerRef) { }
+
+}
